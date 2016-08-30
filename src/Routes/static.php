6cecac347c0db7ecc2646e187add193c851e86e0
Othmane SENOUCI <osenouci@gmail.com>
2016-08-31 00:26:52 +0200

    SLIM no longer serves static pages. Only API calls

--- src/Routes/static.php
+++ src/Routes/static.php
@@ -2,7 +2,14 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
+/*
+
+No static pages are served any more. The project has been divided into two parts:
+1- The API
+2- Stand alone interface that uses CROS requests.
+
 // Define app routes
 $app->get('/', function ($request, $response, $args) {
     return $this->renderer->render($response, "./index.html", $args);
 });
+*/
